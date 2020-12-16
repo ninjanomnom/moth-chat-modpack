432c324d865d1c0ddaf02d09ec55d140cd05d5bc
Emmett Gaines <ninjanomnom@gmail.com>
2020-12-16 08:40:51 -0500

    Initial commit

---
+++ config/Reika/Cave_Definitions/global.lua
@@ -0,0 +1,17 @@
+{
+	type = "global"
+	cave_size = 2.0
+	density = 1.0
+	dungeons = 1.0
+	gen_strongholds = true
+	large_cave_chance = 45.0
+	large_cave_size = 1.2
+	large_cave_variance = 6.0
+	lava_caves = true
+	max_y = 55
+	min_y = 0
+	mineshafts = 1.0
+	ravines = 1.3
+	vscale = 1.0
+	water_caves = false
+}
